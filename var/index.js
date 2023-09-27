47a23499cc0ae9be153ad9602d1d95b818e2ff42
Utkarsh860 <utkarshsachan653@gmail.com>
2023-09-27 23:03:56 +0530

    var

---
+++ var/index.js
@@ -0,0 +1,65 @@
+/*
+// To create a variable in JavaScript, use the var keyword.
+
+var a ; // This step is called variable Declaration.
+a = 10; // The second step is called variable Assignment.
+
+// we can also do these steps in a one line 
+var b = 20;
+
+alert("To create a variable in JavaScript, use the var keyword");
+alert(a+b);
+
+// Note : JS allows you to store a number (10) in a variable which had a string (“Utkarsh”)       →       Dynamically Typed
+
+var temp ; // This step is called variable Declaration.
+temp = 10; // This step is called variable Assignment.
+console.log(temp);
+
+// Note : in the below step, we again declare & assign a same variable name as above  that means var allows both Re-declaration & Re-assignment
+var temp = "Utkarsh"; 
+console.log(temp);
+*/ 
+
+//------------------------------------------------------------------------------------------------//
+
+/*
+// To create a variable in JavaScript, use the let keyword.
+let a ;  // This step is called variable Declaration.
+a = 10; // The second step is called variable Assignment.
+
+// we can also do these steps in a one line 
+let b = 20;
+
+alert("To create a variable in JavaScript, use the let keyword");
+alert(a+b);
+
+let temp; // This step is called variable Declaration.
+temp = 10; // This step is called variable Assignment.
+console.log(temp);
+
+// Note: if we again declare a variable with same variable name means "Re-declaration" then it throws an Error but we can assign different value with same variable means "Re-assignment".
+// let temp = "Utkarsh"; it throws an Error 
+ like :  "SyntaxError: Identifier 'temp' has already been declared." 
+ that means let allows only "Re-assignment" not "Re-declaration"
+temp = "Utkarsh";
+console.log(temp);
+*/
+
+//------------------------------------------------------------------------------------------------//
+
+// /*
+// To create a variable in JavaScript, use the const keyword.
+const a = 10; // This step is called variable Declaration..
+// When a programmer is sure that a variable will never change, they can declare it with const to guarantee and clearly communicate that fact to everyone.
+
+console.log(a);
+// we can also do these steps in a one line
+const b = 20;
+
+alert("To create a variable in JavaScript, use the const keyword");
+alert(a + b);
+
+// */
+
+//------------------------------------------------------------------------------------------------//
