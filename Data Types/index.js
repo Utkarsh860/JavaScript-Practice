b1146e32445c496848e65183538a1417c3889237
Utkarsh860 <utkarshsachan653@gmail.com>
2023-09-29 15:36:28 +0530

    Data Types

--- Data Types/index.js
+++ Data Types/index.js
@@ -20,24 +20,24 @@ Programming languages that allows such things such as JavaScript called "Dynamic
                         double (64-bit floating point).
 */ 
 
-alert(1/0);    // Infinity
-alert(Infinity + 10);  // Infinity 
+// alert(1/0);    // Infinity
+// alert(Infinity + 10);  // Infinity 
 
 /*
 . -Infinity represents a number that is smaller than any real number. 
 */
 
-alert(-1/0);  // -Infinity
-alert(-Infinity + 10);  // -Infinity 
-alert(-Infinity - 10);  // -Infinity 
+// alert(-1/0);  // -Infinity
+// alert(-Infinity + 10);  // -Infinity 
+// alert(-Infinity - 10);  // -Infinity 
 
 /*
 .NaN stands for "Not a Number" which often used to indicate an error or an undefined value in calculations.
 */
 
-alert("not a num"/2);  // NaN
-alert(NaN + 1);  // NaN
-alert(Math.sqrt(-1));  // NaN
+// alert("not a num"/2);  // NaN
+// alert(NaN + 1);  // NaN
+// alert(Math.sqrt(-1));  // NaN
 
 /*
 2. String --> 
@@ -66,8 +66,8 @@ alert(`Hello, ${name}`);
                                          Boolean values also come as a result of comparison. 
 */
 
-alert(4>1);  // true bcoz 4 is greater than 1.
-alert(4<1); // false bcoz 4 is not less than 1.
+// alert(4>1);  // true bcoz 4 is greater than 1.
+// alert(4<1); // false bcoz 4 is not less than 1.
 
 /*
 4. The "null" Value -->
@@ -80,10 +80,10 @@ alert(4<1); // false bcoz 4 is not less than 1.
 
 var age = null;
 // In some mathematical expressions null is work like zero.
-alert(age); // null
-alert(age + 6); // 0 + 6  = 6 
-alert(age*6); // 0*6 = 0 
-alert(1/age); // 1 / 0 = Infinity
+// alert(age); // null
+// alert(age + 6); // 0 + 6  = 6 
+// alert(age*6); // 0*6 = 0 
+// alert(1/age); // 1 / 0 = Infinity
 
 /*
 5. The "undefined" Value -->
@@ -92,9 +92,56 @@ alert(1/age); // 1 / 0 = Infinity
                                               If a variable is declared, but not assigned, then its value is undefined
 */
 var a;
-alert(a); // shows "undefined"
+// alert(a); // shows "undefined"
+
+//--------------------Difference between null & undefined---------------------------//
+/*
+Undefined means a variable has been declared but has yet not been assigned a value.
+
+ Null is an assignment value. It can be assigned to a variable as a representation of no value.
+*/
+//--------------------------------------------------------------------------------------//
+
 
 /*
+6. BigInt --> 
+                    For most purpose  - ( 2 to the power 53 -1 ) to  +(2 to the power 53)
+                    numbers range is quite enough but sometimes we need the entire range of really big integer e.g. for cryptography or microsecond-precision timestamps.
 
+                    It is created by appending n to the end of an integer.
+
+                    Note: BigInt is supported in FireFox/chrome/Edge/Safari but not in IE.
+*/
+
+const a = 15761531235529025629021927n;
+
+
+
+
+/*
+The typeof operator --> 
+                                      The typeof operator returns the type of the operand. It’s useful when we want to process values of different types differently or just want to do a quick check.
+
+                                      A call to typeof x returns a string with the type name.
+*/
+alert(typeof(undefined)) // "undefined"
+
+alert(typeof(0)) // "number"
+
+alert(typeof(10n)) // "bigint"
+
+alert(typeof(true)) // "boolean"
+
+alert(typeof("foo")) // "string"
+
+
+alert(typeof(Math)) // "object"  (1)
+
+alert(typeof(null)) // "object"  (2)
+
+alert(typeof(alert)) // "function"  (3)
+
+
+/*
 <----------    Note : We will discuss the remaining data types separately   ---------->
 */
